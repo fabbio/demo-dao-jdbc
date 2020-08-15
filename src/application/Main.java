575aac2bef8405f9ad2e9fa8df80ddcd97ac1a66
fabbio <fabbio.2k@gmail.com>
2020-08-15 11:46:29 -0300

    add department and main class

---
+++ src/application/Main.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entites.Department;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Department dp = new Department(12,"Fonoaudiologia");
+		
+		System.out.println(dp);
+
+	}
+
+}
