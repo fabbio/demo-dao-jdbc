500cde45e6be8b1b959f840475bcd2b19dad2c5e
fabbio <fabbio.2k@gmail.com>
2020-08-15 12:02:14 -0300

    adicao Seller e Serializable em Departamento

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entites.Department;
+import model.entites.Seller;
 
 public class Main {
 
@@ -9,6 +12,9 @@ public class Main {
 		Department dp = new Department(12,"Fonoaudiologia");
 		
 		System.out.println(dp);
+		
+		Seller seller = new Seller(1,"Jean","Jean@gmail.com",new Date(), 10000.0, dp);
+		System.out.println(seller);
 
 	}
 
